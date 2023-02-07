32dadb58cd612556c4b38f05d4421f28c624c2e1
Eric <eric2umeh@yahoo.com>
2023-02-07 05:24:36 +0100

    Add Header to the dom element with style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './style.css';
+import './img/menu.png';
+import './img/close.png';
+
+import {
+  menuBtn,
+  mobNavGroup,
+  closeBtn,
+  mobileNavlinks,
+} from './modules/docSelectors.js';
+
+// toggle the menu
+menuBtn.addEventListener('click', () => {
+  mobNavGroup.classList.toggle('display-flex');
+});
+
+closeBtn.addEventListener('click', () => {
+  mobNavGroup.classList.remove('display-flex');
+});
+
+mobileNavlinks.forEach((item) => {
+  item.addEventListener('click', () => {
+    mobNavGroup.classList.remove('display-flex');
+  });
+});
